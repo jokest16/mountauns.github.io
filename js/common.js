448fbfcd5de6af85213fdc3fa68601c856164938
jokest16 <jokest16@gmail.com>
2016-09-23 03:26:37 +0300

    commit

---
+++ js/common.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function() {
+	new WOW().init();
+	$('input[type="button"]').on('click',function () {
+		var email = $('input[type="email"]').val();
+		var adr_pattern=/[0-9a-z_-]+@[0-9a-z_]+\.[a-z]{2,5}/i;//
+		if(email==''|| email==" "){
+			alert('Вы некоректно ввели свой email');
+		}else if(adr_pattern.test(email)==false){
+			alert('Вы некоректно ввели свой email');
+		}
+	});
+	var $container = $(".masonry-container");
+		$container.imagesLoaded(function () {
+			$container.masonry({
+				columnWidth: ".item-mos",
+			itemSelector: ".item-mos"
+			});
+		});
+			$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				}
+			}
+		});
+	});
